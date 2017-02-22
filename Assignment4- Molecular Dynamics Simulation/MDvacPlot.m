555f61a3cb11b77f6cac79840f639654d87cd429
anupvkanale <anupvkanale@gmail.com>
2017-02-22 11:46:23 -0800

    report-- vac section complete

--- Assignment4- Molecular Dynamics Simulation/MDvacPlot.m
+++ Assignment4- Molecular Dynamics Simulation/MDvacPlot.m
@@ -4,6 +4,7 @@
 vacData = dlmread('mdOutVac.txt');
 time = vacData(:,1);
 vac = vacData(:,2);
+f = fit(time,vac,'exp1');
 
-plot(time, vac, 'r-', 'LineWidth',2);
+plot(f, time, vac);% 'b.', time, f);
 xlabel('time (LJ unit)'); ylabel('Vel Auto Corr.');
